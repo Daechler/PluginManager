df951e846875ef75b420826cc3a15c70ff2f51a4
Daechler <50748803+Daechler@users.noreply.github.com>
2023-05-25 18:13:00 +0200

    Add files via upload

--- src/main/java/net/daechler/pluginmanager/PluginManager.java
+++ src/main/java/net/daechler/pluginmanager/PluginManager.java
@@ -1,5 +1,6 @@
 package net.daechler.pluginmanager;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,31 +13,33 @@ public class PluginManager extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("PluginManager has been enabled!");
+        // Plugin activation message
+        getLogger().info(ChatColor.GREEN + getName() + " has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("PluginManager has been disabled!");
+        // Plugin disable message
+        getLogger().info(ChatColor.RED + getName() + " has been disabled!");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("pmload")) {
             if (args.length != 1) {
-                sender.sendMessage("Usage: /pmload <plugin_name>");
+                sender.sendMessage(ChatColor.RED + "Usage: /pmload <plugin_name>");
                 return false;
             }
             String pluginName = args[0];
             Plugin plugin = getServer().getPluginManager().getPlugin(pluginName);
             if (plugin != null) {
                 if (plugin.isEnabled()) {
-                    sender.sendMessage(pluginName + " is already loaded!");
+                    sender.sendMessage(ChatColor.RED + pluginName + " is already loaded!");
                     return false;
                 }
                 else {
                     getServer().getPluginManager().enablePlugin(plugin);
-                    sender.sendMessage(pluginName + " has been loaded!");
+                    sender.sendMessage(ChatColor.GREEN + pluginName + " has been loaded!");
                     return true;
                 }
             }
@@ -44,57 +47,57 @@ public class PluginManager extends JavaPlugin {
                 // Check if the plugin is in a JAR file
                 File pluginFile = new File(getDataFolder().getParentFile(), pluginName);
                 if (!pluginFile.exists()) {
-                    sender.sendMessage(pluginName + " not found!");
+                    sender.sendMessage(ChatColor.RED + pluginName + " not found!");
                     return false;
                 }
                 try {
                     plugin = getServer().getPluginManager().loadPlugin(pluginFile);
                     if (plugin != null) {
                         getServer().getPluginManager().enablePlugin(plugin);
-                        sender.sendMessage(pluginName + " has been loaded!");
+                        sender.sendMessage(ChatColor.GREEN + pluginName + " has been loaded!");
                         return true;
                     }
                 } catch (Exception e) {
-                    sender.sendMessage("Failed to load plugin: " + pluginName);
-                    getLogger().log(Level.WARNING, "Failed to load plugin " + pluginName, e);
+                    sender.sendMessage(ChatColor.RED + "Failed to load plugin: " + pluginName);
+                    getLogger().log(Level.WARNING, ChatColor.RED + "Failed to load plugin " + pluginName, e);
                 }
-                sender.sendMessage("Failed to load plugin: " + pluginName);
+                sender.sendMessage(ChatColor.RED + "Failed to load plugin: " + pluginName);
                 return false;
             }
         }
         else if (cmd.getName().equalsIgnoreCase("pmunload")) {
             if (args.length != 1) {
-                sender.sendMessage("Usage: /pmunload <plugin_name>");
+                sender.sendMessage(ChatColor.RED + "Usage: /pmunload <plugin_name>");
                 return false;
             }
             String pluginName = args[0];
             Plugin plugin = getServer().getPluginManager().getPlugin(pluginName);
             if (plugin == null) {
-                sender.sendMessage(pluginName + " not found!");
+                sender.sendMessage(ChatColor.RED + pluginName + " not found!");
                 return false;
             }
             if (!plugin.isEnabled()) {
-                sender.sendMessage(pluginName + " is already unloaded!");
+                sender.sendMessage(ChatColor.RED + pluginName + " is already unloaded!");
                 return false;
             }
             getServer().getPluginManager().disablePlugin(plugin);
-            sender.sendMessage(pluginName + " has been unloaded!");
+            sender.sendMessage(ChatColor.GREEN + pluginName + " has been unloaded!");
             return true;
         }
         else if (cmd.getName().equalsIgnoreCase("pmreload")) {
             if (args.length != 1) {
-                sender.sendMessage("Usage: /pmreload <plugin_name>");
+                sender.sendMessage(ChatColor.RED + "Usage: /pmreload <plugin_name>");
                 return false;
             }
             String pluginName = args[0];
             Plugin plugin = getServer().getPluginManager().getPlugin(pluginName);
             if (plugin == null) {
-                sender.sendMessage(pluginName + " not found!");
+                sender.sendMessage(ChatColor.RED + pluginName + " not found!");
                 return false;
             }
             getServer().getPluginManager().disablePlugin(plugin);
             getServer().getPluginManager().enablePlugin(plugin);
-            sender.sendMessage(pluginName + " has been reloaded!");
+            sender.sendMessage(ChatColor.GREEN + pluginName + " has been reloaded!");
             return true;
         }
         return false;
